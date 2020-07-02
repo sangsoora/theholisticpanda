99e808bed4bf76c79e9626eaee0397fa10d0fa65
Sangsoo Ra <nadang82@gmail.com>
2020-07-02 15:59:32 +0200

    few more modification for models and seeds

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,6 +1,8 @@
 class Practitioner < ApplicationRecord
   belongs_to :user
   has_many :practitioner_languages, dependent: :destroy
-  has_many :practitioner_specialty, dependent: :destroy
+  has_many :practitioner_specialties, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :services, through: :practitioner_specialties, dependent: :destroy
+  has_many :sessions, through: :services
 end
