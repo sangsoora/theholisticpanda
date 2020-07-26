20cc0fda05f05a24b20f9dddbf240132a75196fa
Sangsoo Ra <nadang82@gmail.com>
2020-07-26 16:35:46 +0200

    redirect to currunt page after sign up and sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_location
 
   include Pundit
 
@@ -15,6 +16,28 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone_number])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone_number])
+  end
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    session[:previous_url] || root_path
   end
 
   private
