90b9ecd5bb9a4dfa85e7ef16ac666a1bb6f8c3fc
Sangsoo Ra <nadang82@gmail.com>
2021-02-15 16:11:18 +0100

    back to now

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,12 +3,12 @@ class StripeCheckoutSessionService
     if Practitioner.find_by(checkout_session_id: event.data.object.id)
       @practitioner = Practitioner.find_by(checkout_session_id: event.data.object.id)
       @practitioner.update!(payment_status: 'paid')
-      PractitionerMailer.with(practitioner: @practitioner).welcome.deliver_later
-      AdminMailer.with(practitioner: @practitioner).new_practitioner.deliver_later
+      PractitionerMailer.with(practitioner: @practitioner).welcome.deliver_now
+      AdminMailer.with(practitioner: @practitioner).new_practitioner.deliver_now
     elsif Session.find_by(checkout_session_id: event.data.object.id)
       @session = Session.find_by(checkout_session_id: event.data.object.id)
       @session.update!(status: 'pending', paid: true)
-      SessionMailer.with(session: @session).send_request.deliver_later
+      SessionMailer.with(session: @session).send_request.deliver_now
       Notification.create(recipient: @session.practitioner.user, actor: current_user, action: 'sent you a session request', notifiable: @session)
     end
   end
