e56b147feab55fa9951f9446e8682b91b9d7ffcc
Sangsoo Ra <nadang82@gmail.com>
2021-03-06 18:15:06 +0100

    next try

---
+++ lib/core_extensions/active_storage/representation/downloader.rb
@@ -0,0 +1,20 @@
+module CoreExtensions
+  module ActiveStorage
+    module Representation
+      module Downloader
+        include CoreExtensions::ActiveStorage::Headers
+
+        def show
+          return super unless params[:proxy] == 'true'
+          set_headers(@blob)
+          variant = @blob.representation(params[:variation_key]).processed
+          @blob.service.download(variant.key) do |chunk|
+            response.stream.write(chunk)
+          end
+        ensure
+          response.stream.close
+        end
+      end
+    end
+  end
+end
