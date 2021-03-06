13c36dd42d1d32e7d511b39d66a0ba0c8f4dda7b
Sangsoo Ra <nadang82@gmail.com>
2021-03-06 18:19:58 +0100

    next try

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Theholisticpanda
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
-
+    config.eager_load_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
