1a99398972fb8d687d0690aa2e2a2c4714fc90b6
Sangsoo Ra <nadang82@gmail.com>
2021-06-03 15:49:49 +0200

    added more model for admin panel and added capacity to events

--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -1,5 +1,5 @@
 class EventAttendee < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :event
   validates_uniqueness_of :user, scope: [:event]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_220129) do
+ActiveRecord::Schema.define(version: 2021_06_03_134815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_220129) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "capacity"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
