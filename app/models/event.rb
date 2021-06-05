eb5d194a1d2ac0b21487f0dacc7c98d8e8625580
Sangsoo Ra <nadang82@gmail.com>
2021-06-05 21:10:43 +0200

    added event crate in admin panel, event design and updated date format

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :event_attendees
+  has_many :event_attendees, dependent: :destroy
+  delegate :practitioner, to: :user, allow_nil: true
 
   has_one_attached :photo
 end
