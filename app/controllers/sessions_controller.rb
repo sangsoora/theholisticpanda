6f0d64ca7035c925890d8f9fd9aacba321e5c259
Sangsoo Ra <nadang82@gmail.com>
2021-06-22 02:51:04 +0200

    promo fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -71,7 +71,7 @@ class SessionsController < ApplicationController
   def update
     if params[:commit] == 'Confirm payment method'
       if @session.update(session_params)
-        UserPromo.find_by(promo_id: params[:session][:promo_id]).update(active: false) if params[:session][:promo_id]
+        UserPromo.find_by(promo_id: params[:session][:promo_id]).update(active: false) if params[:session][:promo_id] && params[:session][:promo_id] != ''
         @session.update!(status: 'pending')
         SessionMailer.with(session: @session).send_request.deliver_now
         Notification.create(recipient: @session.practitioner.user, actor: current_user, action: 'sent you a session request', notifiable: @session)
