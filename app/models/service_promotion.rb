1dd5135b883f0f908817ad4bf11948c26975f7b2
Sangsoo Ra <nadang82@gmail.com>
2021-11-19 20:55:27 +0100

    updated validation for service promotion and sign up path

--- app/models/service_promotion.rb
+++ app/models/service_promotion.rb
@@ -1,3 +1,16 @@
 class ServicePromotion < ApplicationRecord
   belongs_to :service
+
+  validates_presence_of :start_date, :end_date, :discount_percentage
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date")
+    end
+  end
 end
