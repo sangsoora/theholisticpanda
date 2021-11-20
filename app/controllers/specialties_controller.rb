5e5e2dfd4b5b7050d046aebe5332c5cc62246171
Sangsoo Ra <nadang82@gmail.com>
2021-11-20 21:35:33 +0100

    price to show 2 decimal points and added remaining days for service promotions

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,6 +1,7 @@
 class SpecialtiesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show filter]
   before_action :set_specialty, only: %i[show destroy]
+  before_action :set_notifications, only: [:show]
 
   def show
     @specialty_services = @specialty.services.active_services
@@ -31,6 +32,10 @@ class SpecialtiesController < ApplicationController
   def set_specialty
     @specialty = Specialty.find(params[:id])
     authorize @specialty
+  end
+
+  def set_notifications
+    @notifications = Notification.includes(:actor).where(recipient: current_user).order('created_at DESC').unread
   end
 
   def specialty_params
