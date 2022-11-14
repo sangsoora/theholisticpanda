11c0074aed82dbb88b8c478f61f3d4444b540463
Sangsoo Ra <nadang82@gmail.com>
2022-11-14 19:13:13 +0100

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.active_record.use_yaml_unsafe_load = true
 end
